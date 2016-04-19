114eaaaf3aab8e532c1cdcf48a35e4867b76a573
dvdhoek <djpvanderhoek@gmail.com>
2016-04-19 17:31:49 +0800

    added css

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -6,10 +6,8 @@ get '/' do
 end
 
 post '/urls' do
-	# byebug
 	long_url = params[:long_url]
 	short_url = Url.shorten
-	
 	@url = Url.new(long_url: long_url, short_url: short_url)
 	@url.save
 	redirect '/urls'
@@ -23,12 +21,15 @@ end
 get '/:short_url' do
 	a = Url.find_by short_url: params[:short_url]
 	a.click_count = a.click_count + 1
-	byebug
 	a.save
 	link = a.long_url
-	
 	redirect link
 end
+
+post '/back' do
+	redirect '/'
+end
+
 
 # a get request is the request to access the page at /... 
 # .erb on the end of url allows to write ruby inside of html file (embedded ruby)
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,5 +1,5 @@
 class Url < ActiveRecord::Base
-	validates :long_url, :format => URI::regexp(%w(http https))
+	# validates :long_url, :format => URI::regexp(%w(http https))
 	def self.shorten
 		short_url = ([*('A'..'Z'),*('0'..'9')]-%w(0 1 I O)).sample(8).join
 	end
